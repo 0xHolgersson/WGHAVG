bc5c655e41d4b3c12233ae5b4860f59e7af449b7
0xHolgersson <anton.h.ekstrand@gmail.com>
2026-03-01 04:11:50 +0100

    Update weighted_average_example.ino

--- Examples/weighted_average_example/weighted_average_example.ino
+++ Examples/weighted_average_example/weighted_average_example.ino
@@ -8,10 +8,26 @@ weighted average example placeholder
 
 void setup() {
   // put your setup code here, to run once:
-
+  tWGHAVG WGHAVGa(WGHAVG_Fx_Linear, 1, 1, 5);
+  tWGHAVG WGHAVGb(WGHAVG_Fx_Exponential, 1, 1, 5);
+  tWGHAVG WGHAVGc(WGHAVG_Fx_Cosine, 1, 1, 5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-while(1){}
+  WGHAVGa.addValue();
+  WGHAVGb.addValue();
+  WGHAVGc.addValue();
+
+  Serial.print("step:");
+  Serial.print(potentiometer);
+  Serial.print(",");
+  Serial.print("WGHAVGa:");
+  Serial.println(WGHAVGa.getResult());
+  Serial.print(",");
+  Serial.print("WGHAVGb:");
+  Serial.println(WGHAVGb.getResult());
+  Serial.print(",");
+  Serial.print("WGHAVGc:");
+  Serialln.println(WGHAVGac.getResult());
 }
